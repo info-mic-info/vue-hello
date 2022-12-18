82a51aed75bf3d7c30dde3c187bcb11e74c7d83c
info-mic-info <jobsocial94@gmail.com>
2022-12-18 22:34:10 +0100

    Complete

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -6,7 +6,8 @@ const {
 createApp({
     data(){
         return{
-            message : `Hello`
+            message : null,
+            url: `https://images.unsplash.com/photo-1493119508027-2b584f234d6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80`
         }
     }
 }).mount('#app')
